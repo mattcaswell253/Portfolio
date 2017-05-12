896636ec3c935786fabf0a9b121d9539846e40e6
mattcaswell <matthewcaswell253@Gmail.com>
2017-05-12 16:55:51 -0700

    set up API

--- src/Portfolio/Models/Portfolio.cs
+++ src/Portfolio/Models/Portfolio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -13,5 +14,12 @@ namespace Portfolio.Models
         [Key]
         public string Body { get; set; }
         public string User { get; set; }
+
+        public static List<Portfolio> GetProjects()
+        {
+            var client = new RestClient("https://api.github.com");
+            var request = new RestRequest("/users/mattcaswell253/starred", Method.GET);
+            request.AddHeader("Accept", "application/vnd.github.v3+json");
+        }
     }
 }
