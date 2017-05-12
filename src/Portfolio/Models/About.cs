a8b3d39b544fceedb46adac68ad1dc74c4510398
mattcaswell <matthewcaswell253@Gmail.com>
2017-05-12 13:02:12 -0700

    create initial files and folders as well as models views and controllers folders

---
+++ src/Portfolio/Models/About.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Models
+{
+    [Table("About")]
+    public class About
+    {
+        [Key]
+        public int AboutId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        
+
+    }
+}
---
+++ src/Portfolio/Models/PortfolioContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Models
+{
+    public class PortfolioContext : IdentityDbContext<User>
+    {
+        public PortfolioContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public virtual DbSet<About> About { get; set; }
+        public virtual DbSet<Portfolio> Portfolio { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Portfolio;integrated security=True;");
+        }
+    }
+}
